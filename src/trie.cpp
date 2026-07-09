cc4a1e12facf2de1eb59e98b91065c0f3a81ef84
Warrick Brugal <warry.extraz@gmail.com>
2026-07-09 16:42:17 -0400

    Feture: Added autocomplete functionality + error-handling, Trie destructor, and added Trie files to Cmake lists

--- src/trie.cpp
+++ src/trie.cpp
@@ -1,12 +1,28 @@
 #include "trie.h"
 #include <cctype>
 
+Trie::~Trie()
+{
+    for (int i = 0; i < 26; i++) {
+        if(root->children[i] != nullptr) {
+            delete root->children[i];
+            root->children[i] = nullptr;
+        }
+    }
+
+    delete root;
+    root = nullptr;
+
+    delete this;
+}
+
 void Trie::insert(const string& word)
 {
     TrieNode* current = root;
 
     for (char c : word) {
         int index = static_cast<char>(tolower(c)) - 'a'; //find the index of the child node
+        if(index < 0 || index >= 26) continue; //skip invalid characters
 
         //if the child node at the index in the children array is null, create a new node at that index
         if (current->children[index] == nullptr) {
@@ -27,11 +43,10 @@ bool Trie::search(const string& word)
 
     for (char c : word) {
         int index = static_cast<char>(tolower(c)) - 'a'; //find the index of the child node
+        if(index < 0 || index >= 26) continue; //skip invalid characters
 
         //if the child node at the index in the children array is null
-        if (current->children[index] == nullptr) { 
-            return false;
-        }
+        if (current->children[index] == nullptr) return false;
 
         //if not null, move current ptr to the child node
         current = current->children[index];
@@ -41,7 +56,43 @@ bool Trie::search(const string& word)
     return current->isWord;
 }
 
-void Trie::automplete(const string &prefix, vector<string> &results)
+vector<string> Trie::autocomplete(const string& prefix) //return a list of strings that start with the given prefix
 {
-    
+    vector<string> results;
+    if(prefix.empty() || root == nullptr) return results;
+
+    //find the node with the prefix
+    TrieNode* current = root;
+    for (char c : prefix) {
+        int index = static_cast<char>(tolower(c)) - 'a';
+        if (index < 0 || index >= 26) continue;
+
+        //if the child node at the index in the children array is null
+        if (current->children[index] == nullptr) return results;
+
+        //move the current pointer to the child node
+        current = current->children[index];
+    }
+
+    //call the helper function to find all words with the given prefix
+    autocompleteHelper(prefix, current, results);
+    return results;
+}
+
+void Trie::autocompleteHelper(const string& prefix, TrieNode* node, vector<string>& results)
+{
+    if(node == nullptr) return;
+
+    // check if the current node is a complete word and add it to the result vector
+    if (node->isWord) {
+        results.push_back(prefix);
+    }
+
+    //recursively explore all child nodes for each letter in the alphabet using DFS
+    for (int i = 0; i < 26; i++) {
+        if (node->children[i] != nullptr) {
+            char c = 'a' + i;
+            autocompleteHelper(prefix + c, node->children[i], results);
+        }
+    }
 }
--- src/trie.h
+++ src/trie.h
@@ -23,8 +23,11 @@ private:
 
 public:
     Trie() : root(new TrieNode()) {};
-
+    ~Trie();
+    
     void insert(const string& word);
     bool search(const string& word);
-    void automplete(const string& prefix, vector<string>& results);
+
+    vector<string> autocomplete(const string& prefix);
+    void autocompleteHelper(const string& prefix, TrieNode* node, vector<string>& results);
 };
